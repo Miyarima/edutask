704a3c43e33940bcd7118c980569ab0b87360c61
axkl20 <115080818+axkl20@users.noreply.github.com>
2024-04-18 11:05:43 +0200

    test 1 assignment 3

---
+++ backend/test/lab1/test_create.py
@@ -0,0 +1,54 @@
+import pytest
+import unittest.mock as mock
+from unittest.mock import patch
+
+# from src.controllers.usercontroller import UserController
+# import src.util.dao as dao
+from src.util.dao import DAO
+
+
+# @pytest.fixture
+# @patch('src.util.helpers.UserController', autospec=True)
+
+@pytest.mark.lab1
+@pytest.mark.parametrize('test_dict', [({"name": "John Doe", "age": 30,"is_active": True, "data": "alex is a laundry man."})])
+def test_create(test_dict: dict):
+    dao_class = DAO("user")
+    dao_class.create(dao_class, test_dict)
+
+# @pytest.mark.lab1
+# @pytest.mark.parametrize('email, expected', [({'email': 'Janedoe@example.com'}, 'Janedoe@example.com')])
+# def test_valid_email(email, expected):
+#     mocked_usercontroller = mock.MagicMock()
+#     mocked_usercontroller.find.return_value = [email]
+#     sut = UserController(mocked_usercontroller)
+#     valid_result = sut.get_user_by_email('Janedoe@example.com')
+#     assert valid_result["email"] == expected
+
+# @pytest.mark.lab1
+# @pytest.mark.parametrize('email, expected', [([{'id': 28, 'name': 'Axel', 'email': 'Janedoe@example.com'},
+#     {'id': 42, 'name': 'Jonathan', 'email': 'Janedoe@example.com'},
+#     {'id': 69, 'name': 'Charlie', 'email': 'Janedoe@example.com'}], {'id': 28, 'name': 'Axel', 'email': 'Janedoe@example.com'})])
+# def test_multiple_valid_emails(email, expected):
+#     mocked_usercontroller = mock.MagicMock()
+#     mocked_usercontroller.find.return_value = email
+#     sut = UserController(mocked_usercontroller)
+#     valid_result = sut.get_user_by_email('Janedoe@example.com')
+#     assert valid_result == expected
+
+# @pytest.mark.lab1
+# @pytest.mark.parametrize('email, expected', [({}, None)])
+# def test_no_user_associated(email, expected):
+#     mocked_usercontroller = mock.MagicMock()
+#     mocked_usercontroller.find.return_value = [email]
+#     sut = UserController(mocked_usercontroller)
+#     valid_result = sut.get_user_by_email('Janedoe@example.com')
+#     assert valid_result == expected
+
+# @pytest.mark.lab1
+# @pytest.mark.parametrize('email, expected', [({'email': 'Janedoeexample.com'}, ValueError)])
+# def test_invalid_email(email, expected):
+#     mocked_usercontroller = mock.MagicMock()
+#     sut = UserController(mocked_usercontroller)
+#     with pytest.raises(expected):
+#         sut.get_user_by_email(email["email"])
