21ce2f825005adac65a10dba45130a96b2c29f22
Jonathan Göransson <jonathan.a.goransson@outlook.com>
2024-04-23 17:18:43 +0200

    added the remaning test cases

--- frontend/cypress/e2e/tasks.cy.js
+++ frontend/cypress/e2e/tasks.cy.js
@@ -19,46 +19,95 @@ describe ("Add task to todo list", () => {
                 name = user.firstName + ' ' + user.lastName
                 email = user.email
             })
+            .then(() => {
+                cy.visit('http://localhost:3000')
+                cy.contains('div', 'Email Address')
+                    .find('input[type=text]')
+                    .type(email)
+        
+                cy.get('form')
+                    .submit()
+        
+                cy.get('h1')
+                    .should('contain.text', 'Your tasks, ' + name)
+                
+                cy.get('form')
+                    .should('contain.text', 'Title')
+        
+                cy.contains('div', 'Title')
+                    .find('input[type=text]')
+                    .type(task)
+        
+                cy.get('form')
+                    .submit()
+                
+                cy.get('p')
+                    .should('contain.text', 'Here you can find your 1 task. Click on each thumbnail in the list to add, update, or delete the todo items you have associated to this video.')
+            })
         })
+
     })
 
     beforeEach(function () {
         cy.visit('http://localhost:3000')
         cy.contains('div', 'Email Address')
             .find('input[type=text]')
-            .type(email)
+            .type(email);
 
         cy.get('form')
-            .submit()
-
-        cy.get('h1')
-            .should('contain.text', 'Your tasks, ' + name)
+            .submit();
+        
+        cy.contains("div", "a")
+            .click();
     })
 
-    it('Finding form and adding one task without text', () => {
-        cy.get('form')
-            .should('contain.text', 'Title')
-
+    it('Add new todo without text', () => {
         cy.get('form')
             .find('input[type=submit]')
-            .should("be.disabled")
+            .should("be.disabled");
     })
 
-    it('Finding form and adding one task', () => {
-        cy.get('form')
-            .should('contain.text', 'Title')
-
-        cy.contains('div', 'Title')
+    it('Add new todo with text', () => {
+        cy.get('.popup')
             .find('input[type=text]')
-            .type(task)
-
-        cy.get('form')
-            .submit()
+            .type("customer meeting at 1");
         
-        cy.get('p')
-            .should('contain.text', 'Here you can find your 1 task. Click on each thumbnail in the list to add, update, or delete the todo items you have associated to this video.')
+        cy.get('.inline-form')
+            .submit();
+        
+        cy.get('.todo-list')
+            .should("contain.text", "customer meeting at 1");
     })
 
+    it('Set an active task as done and unmark it', () => {
+        cy.contains('.todo-item', 'customer meeting at 1')
+            .get('.unchecked')
+            .last()
+            .click();
+        
+        cy.contains('.todo-item', 'customer meeting at 1')
+            .find('.checked')
+            .should("have.class", "checked");
+
+        cy.contains('.todo-item', 'customer meeting at 1')
+            .find('.checked')
+            .click();
+
+        cy.contains('.todo-item', 'customer meeting at 1')
+            .find('.unchecked')
+            .should("have.class", "unchecked");
+    })
+
+    it('Remove added item in todo list', () => {
+        cy.contains('.todo-item', 'customer meeting at 1')
+            .find('.remover')
+            .last()
+            .click();
+    
+        cy.get(".todo-list")
+            .children()
+            .should("have.length", 2); // 2 and not 1 beacuse for some reason is the form inside the todo-list
+    })
 
     after(function () {
         // clean up by deleting the user from the database
